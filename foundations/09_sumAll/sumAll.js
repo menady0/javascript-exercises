bdf89c3a7562873c9395e1a0ecad0019a44dac71
Mina <menady20@gmail.com>
2026-06-29 08:14:32 +0300

    Solve 09

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function (numA, numB) {
+  if (
+    Math.floor(numA) !== numA ||
+    Math.floor(numB) !== numB ||
+    numA < 0 ||
+    numB < 0
+  )
+    return "ERROR";
+  let large = numA >= numB ? numA : numB;
+  let small = numA >= numB ? numB : numA;
+  let sum = 0;
+  for (let i = small; i <= large; i++) {
+    sum += i;
+  }
+  return sum;
 };
-
 // Do not edit below this line
 module.exports = sumAll;
