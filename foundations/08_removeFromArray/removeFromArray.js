2a3cf4eafbc155a24ecd866a972f2ff1f9dc5d23
Mina <menady20@gmail.com>
2026-06-29 08:01:59 +0300

    Solve 08

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...items) {
+  // arr.splice(arr.indexOf(element), 1);
+  for (const element of items) {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] === element) {
+        arr.splice(i, 1);
+        i--;
+      }
+    }
+  }
+  return arr;
 };
-
+//  0, 1, 2, 3
+// [1, 2, 2, 3]
 // Do not edit below this line
 module.exports = removeFromArray;
