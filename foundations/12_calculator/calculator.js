e868e7719996bd56f3ac23c30c4bbd1df1dc06a9
Mina <menady20@gmail.com>
2026-06-29 09:11:10 +0300

    Solve 12

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  return (arr.length !== 0) ? arr.reduce((total, current) => (total += current)) : 0;
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  return (arr.length !== 0) ? arr.reduce((total, current) => (total *= current)) : 0;
 };
 
-const power = function() {
-	
+const power = function (base, exponent) {
+  return Math.pow(base, exponent)
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let total = 1;
+  for(let i = 1; i <= num; i++){
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
@@ -29,5 +33,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
